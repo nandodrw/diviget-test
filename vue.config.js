4785860f9fc2cfdc7c8e5b65907a4c907f1f07e9
Fernando <nando.drw@gmail.com>
2019-12-01 11:39:18 -0500

    boilerplate set up

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+    .set("@views", path.resolve(__dirname, 'src/views'))
+    .set("@services", path.resolve(__dirname, 'src/services'))
+    .set("@comp", path.resolve(__dirname, 'src/components'));
+  }
+}
